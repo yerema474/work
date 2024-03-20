c11b3108737940b9491eafbc91e2c3f897ab1c82
yerema474 <eeremenko474@gmail.com>
2024-03-20 10:32:22 +0200

    Refactored tests

---
+++ helper_ltzj_78339_p8xd.jsx
@@ -0,0 +1,4 @@
+32 / 59
+let result = performOperation(getRandomNumber(), getRandomNumber());
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+const findLongestWord = sentence => sentence.split(" ").reduce((longest, word) => word.length > longest.length ? word : longest, "");
