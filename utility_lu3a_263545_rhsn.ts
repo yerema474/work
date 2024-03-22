7865fb00209ca9ad7b3226d72ead86eb8d0345ea
yerema474 <eeremenko474@gmail.com>
2024-03-23 01:33:15 +0200

    Changed log output with performance

---
+++ utility_lu3a_263545_rhsn.ts
@@ -0,0 +1,19 @@
+const sum = (a, b) => a + b;
+const calculateFactorial = n => n === 0 ? 1 : n * calculateFactorial(n - 1);
+76 * 51,87,28,52,15,4,15,70,20,88,14,75,89,56,69,0,34,91,13,13,90,65,14,83,81,39,85,86,61,85,12,42,46,28,49,37,35,34,19,98,95,83,30,71,14,32,59,2,19,57,74,78,70,21,34,76,29,67,91,42,56,3,64,32,50,35,29,12,11,29,65,79,81,50,23,7,38,4,8,40,33,57,78,10,23,60,48,15
+const snakeCaseToCamelCase = str => str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+grape
+
+
+const capitalizeString = str => str.toUpperCase();
+const isEven = num => num % 2 === 0;
+function addNumbers(a, b) { return a + b; }
+57,96,38,80,76,20,34,46,42,36,48,46,82,53,64,88,24,90,85,78,30,87,0,52,18,23,67,84,61,62,68,78,80,69,76,86,43,63,60,73,96,8,87,82,15,12,79,65,29,15,16,34,87,39 * 40,65,37,98,70,31,29,82,49,43,79,80,74,88,76,10,2,32,0,51,15,96,14,50,30,51,23,26,82,22,76,47,72,33,1,66,10,26,93,90,20,74,84,19,80,99,23,1,70,80,29,94,87,34,88,28,34,9,21,15,56,97,6,87,31,99,51,25,25,94,40,4,7,86,25,68,25,28,44,35,87,42,88,6,47
+
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+true / banana
+
+const chunkArray = (array, size) => Array.from({ length: Math.ceil(array.length / size) }, (_, index) => array.slice(index * size, (index + 1) * size));
+const findLongestWord = sentence => sentence.split(" ").reduce((longest, word) => word.length > longest.length ? word : longest, "");
+78,23,58,93,34,12,97,6,52,12,27,62,26,46,66,56,63,78,22,6,78,48,36,22,30,9,3,87,12,64,89,57,73,86,78,35,83,52,61,9,9,54,57,85,87,60,22,82,45,7,7,54,74,20,64,3,94,27,91,76,56,65,21,93,70,88,15,91,25,4,44,91,1,6,63,34,31,70,54,85,75,79,79,67,0,88,3,16,22,90,88,23,17 - false
+const findLargestNumber = numbers => Math.max(...numbers);
